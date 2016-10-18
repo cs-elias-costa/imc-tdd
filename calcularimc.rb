8aaef15cb37491ee1689002162e9d668731f7104
Elias Costa <elias.costa@concrete.com.br>
2016-10-18 14:45:01 -0200

    Adicionando os arquivos .rb

---
+++ calcularimc.rb
@@ -0,0 +1,16 @@
+class CalcularImc
+
+  def Peso(peso)
+    return  45.00
+  end
+  def Alt(altura)
+    return 17.00
+  end
+
+
+  def Imc(peso,altura)
+    return  (peso/(altura*altura)).round(2)
+  end
+
+
+end
